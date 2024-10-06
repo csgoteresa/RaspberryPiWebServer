dac4979e1079944b6785724c9063c0b70f0c08bd
Yash Keshri <125530408+csgoteresa@users.noreply.github.com>
2024-10-06 20:54:27 +0530

    boot.py

--- boot.py
+++ boot.py
@@ -2,20 +2,20 @@
 import network, utime
 
 # Replace the following with your WIFI Credentials
-SSID = "<PLACE_YOUR_SSID_HERE>"
-SSI_PASSWORD = "<PLACE_YOUR_WIFI_PASWORD_HERE>"
+SSID = "<WiFi SSID>"
+SSI_PASSWORD = "<WiFi Password>"
 
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('Connecting to the Network')
         sta_if.active(True)
         sta_if.connect(SSID, SSI_PASSWORD)
         while not sta_if.isconnected():
             pass
-    print('Connected! Network config:', sta_if.ifconfig())
+    print('Connected. Network Config:', sta_if.ifconfig())
     
-print("Connecting to your wifi...")
+print("Connecting to your WiFi...")
 do_connect()
 
